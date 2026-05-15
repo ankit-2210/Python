274857075585624e789c98cc3d354a7ae50b80ba
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2026-05-15 11:09:41 +0530

    added python topics

--- 55. InstanceVariables.py
+++ 55. InstanceVariables.py
@@ -1,38 +1,57 @@
-# Instance variable:
-# "Instance variables are the variables for which the value of the variable is different for every instance."
-#
-# We can also say that the value is different for every object that we create. Let us dive into some in-depth explanation. When we create a class, we define a few variables along with it. For example, we have created a class of Students, and we have defined a variable age.
-# All the students cannot have the same age in a class, so we have assigned the variable an average age of 16.  Now, whenever we use an object to print the value of age, it will show 16. We can try to change the value of age, but it will create a new instance variable for the specific object that we are updating it for, hence defining the value to it.
+# Class Variables
+# Class variables are defined at the class level and are shared among all
+# instances of the class. They are defined outside of any method and are
+# usually used to store information that is common to all instances of the
+# class. For example, a class variable can be used to store the number of
+# instances of a class that have been created.
+class MyClass:
+    class_variable = 0  # class variable
+
+    def __init__(self):
+        MyClass.class_variable += 1
+
+    def print_class_variable(self):
+        print(MyClass.class_variable)
 
 
-# Class variable:
-# "Class attributes are owned by the class directly, which means that they are not tied to any object or instance."
-#
-#  Same as in the above example, if we want to change the age for every instance from 16 to 17, then we can do it by using the class variable, which in this case is Student.
-#
-# The code for changing age using a class variable will be something like this:
-# Students.age = 18
+obj1 = MyClass()
+obj2 = MyClass()
+
+obj1.print_class_variable()
+obj2.print_class_variable()
+
+# In the example above, the class_variable is shared among all instances of
+# the class MyClass. When we create new instances of MyClass, the value of
+# class_variable is incremented. When we call the print_class_variable method
+# on obj1 and obj2, we get the same value of class_variable.
+
+# Instance Variables
+# Instance variables are defined at the instance level and are unique to each
+# instance of the class. They are defined inside the init method and are
+# usually used to store information that is specific to each instance of the
+# class. For example, an instance variable can be used to store the name of an
+# employee in a class that represents an employee.
 
 
-class Employee:
-    no_of_leaves = 8
-    pass
+class MyClass1:
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print(self.name)
 
 
-harry = Employee()
-rohan = Employee()
+obj3 = MyClass1("John")
+obj4 = MyClass1("Jane")
 
-harry.name = "Harry"
-harry.salary = 455
-harry.role = "Instructor"
+obj3.print_name()
+obj4.print_name()
 
-rohan.name = "Rohan"
-rohan.salary = 4554
-rohan.role = "Student"
+# In the example above, each instance of the class MyClass has its own value
+# for the name variable. When we call the print_name method on obj1 and obj2,
+# we get different values for name.
 
-
-print(Employee.no_of_leaves)
-print(Employee.__dict__)
-Employee.no_of_leaves = 9  # Override it
-print(Employee.__dict__)
-print(Employee.no_of_leaves)
+# In summary, class variables are shared among all instances of a class and
+# are used to store information that is common to all instances. Instance
+# variables are unique to each instance of a class and are used to store
+# information that is specific to each instance.
